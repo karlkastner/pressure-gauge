39d5e8492fd741d1e464ae7d8a06cc8e24d02726
karlkastner <kastner.karl@gmail.com>
2019-10-20 17:26:10 +0800

    added GPL header

--- @PressureGauge/PressureGauge.m
+++ @PressureGauge/PressureGauge.m
@@ -22,6 +22,20 @@
 % TODO slot should be an object of its own
 % TODO replacement information should be loaded from files
 % TODO make replacement a flag
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <https://www.gnu.org/licenses/>.
+%
 classdef PressureGauge < handle
 	properties (Constant)
 		% limits
